92d128bdfab8493e2737ad926adcf00fecff85cf
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-12-11 11:09:01 +0700

    Add Initial Project

---
+++ app/src/main/java/com/jafar/smarttrash/PreviewActivity.kt
@@ -0,0 +1,11 @@
+package com.jafar.smarttrash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
